c071299a1999bb56ef496f73a3e62ff62e1f8373
Erik the Viking <erik@hhd.com.au>
2018-01-18 16:37:53 +1100

    add static controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     root to: "paintings#index"
   end
 
+  root to: "static#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
